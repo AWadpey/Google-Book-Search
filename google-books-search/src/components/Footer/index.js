00219bd54606e2faaf233e868cc053f8dd08aae2
MistahSanD <wadpey@gmail.com>
2019-03-08 18:26:41 -0800

    jumbo, form, footer, and card components built

---
+++ google-books-search/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <hr />
+            <p className="pull-right">
+                <i classname="fab fa-crown" /> Built by your friendly neighborhood developer, Ashkan Wadpey!
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
